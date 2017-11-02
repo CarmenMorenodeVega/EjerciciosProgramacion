45f980d45868fea852c4492926b07cace1edef8b
Carmen Moreno de Vega <32536552+CarmenMorenodeVega@users.noreply.github.com>
2017-11-02 09:25:07 +0100

    Update Ejercicio16.java

--- EjerciciosT5/Ejercicio16.java
+++ EjerciciosT5/Ejercicio16.java
@@ -1,7 +1,7 @@
 /**
  * T5. Bucles
  *
- * 16. Escribe un programa que diga si un número introducido por    	   teclado es o no primo. 
+ * 16. Escribe un programa que diga si un número introducido por teclado es o no primo. 
        Un número primo es aquel que sólo es divisible entre él mismo
        y la unidad.
  *
