dde852d8fa02dd88e988c388de0af5eec5cb4d8b
Carmen Moreno de Vega <32536552+CarmenMorenodeVega@users.noreply.github.com>
2018-04-10 09:33:30 +0200

    Update ArrayListDiscos.java

--- tema10/arraylistdiscos/ArrayListDiscos.java
+++ tema10/arraylistdiscos/ArrayListDiscos.java
@@ -2,7 +2,7 @@
 vez una lista para almacenar la información sobre los discos en lugar de un 
 array convencional. Comprobarás que el código se simplifica notablemente.
 ¿Cuánto ocupa el programa original hecho con un array? ¿Cuánto ocupa este nuevo
-programahechoconunalista?
+programa hecho con una lista?
  * @author cmove */
 package arraylistdiscos;
 import static java.lang.System.out;
